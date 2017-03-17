f58578f1e2758dd240ffa64deabcb2b3ffc55ea1
elghobaty <elghobaty@gmail.com>
2017-03-17 16:29:14 +0200

    Log Guzzle exceptions fully

--- common/Exceptions/Handler.php
+++ common/Exceptions/Handler.php
@@ -1,6 +1,8 @@
 <?php namespace Common\Exceptions;
 
 use Exception;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
@@ -9,6 +11,7 @@ use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
 {
+
     /**
      * A list of the exception types that should not be reported.
      *
@@ -26,19 +29,29 @@ class Handler extends ExceptionHandler
      *
      * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
      *
-     * @param  \Exception  $e
+     * @param \Exception $e
      * @return void
      */
     public function report(Exception $e)
     {
         parent::report($e);
+        if (is_a($e, ClientException::class)) {
+            /** @type ClientException $e */
+            try {
+                /** @type \Psr\Log\LoggerInterface $logger */
+                $logger = app('Psr\Log\LoggerInterface');
+            } catch (Exception $ex) {
+                throw $e; // throw the original exception
+            }
+            $logger->debug("Full Response: " . $e->getResponse()->getBody()->getContents() . "\n");
+        }
     }
 
     /**
      * Render an exception into an HTTP response.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $e
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Exception               $e
      * @return \Illuminate\Http\Response
      */
     public function render($request, Exception $e)
