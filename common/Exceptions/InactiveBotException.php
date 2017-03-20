dbe51c04032670174e783b15f89b0f53df32b118
elghobaty <elghobaty@gmail.com>
2017-03-20 16:23:52 +0200

    Introducing custom bot exceptions

---
+++ common/Exceptions/InactiveBotException.php
@@ -0,0 +1,6 @@
+<?php namespace Common\Exceptions;
+
+class InactiveBotException extends DisallowedBotOperation
+{
+
+}
---
+++ common/Exceptions/InvalidBotAccessTokenException.php
@@ -0,0 +1,6 @@
+<?php namespace Common\Exceptions;
+
+class InvalidBotAccessTokenException extends DisallowedBotOperation
+{
+
+}
