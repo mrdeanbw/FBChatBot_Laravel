6d0c1f80331abffeb87d2bc8abc06adeece9a0fc
elghobaty <elghobaty@gmail.com>
2017-03-21 20:29:55 +0200

    Clean up code + bot enabled status in page transformer

--- common/Transformers/PageTransformer.php
+++ common/Transformers/PageTransformer.php
@@ -8,11 +8,12 @@ class PageTransformer extends BaseTransformer
     public function transform(Page $page)
     {
         return [
-            'id'         => $page->id,
-            'name'       => $page->name,
-            'avatar_url' => $page->avatar_url,
-            'url'        => $page->url,
-            'bot_id'     => isset($page->bot)? $page->bot->id : null
+            'id'          => $page->id,
+            'name'        => $page->name,
+            'avatar_url'  => $page->avatar_url,
+            'url'         => $page->url,
+            'bot_id'      => isset($page->bot)? $page->bot->id : null,
+            'bot_enabled' => isset($page->bot)? $page->bot->enabled : null,
         ];
     }
 }
